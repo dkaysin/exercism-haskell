bbeb68f90257dcbf03f01a7251a6e84d38245885
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2020-01-14 18:17:42 +0300

    Add daily solutions

--- kindergarten-garden/src/Garden.hs
+++ kindergarten-garden/src/Garden.hs
@@ -4,16 +4,38 @@ module Garden
     , lookupPlants
     ) where
 
+import           Data.List  (transpose)
+import           Data.Map   (Map)
+import qualified Data.Map   as M
+import           Data.Maybe (fromMaybe)
+
 data Plant = Clover
            | Grass
            | Radishes
            | Violets
            deriving (Eq, Show)
 
-data Garden = Dummy
+data Garden = Garden (Map String [Char])
+  deriving (Show)
 
 garden :: [String] -> String -> Garden
-garden students plants = error "You need to implement this function."
+garden students = Garden . M.fromList . zip students . map concat . transpose . map chunks . lines
+
+chunks :: String -> [String]
+chunks [] = []
+chunks xs = take 2 xs : chunks (drop 2 xs)
 
 lookupPlants :: String -> Garden -> [Plant]
-lookupPlants student garden = error "You need to implement this function."
+lookupPlants student (Garden gdn) = fromMaybe [] plantsMaybe
+  where
+    plantsMaybe = M.lookup student gdn >>= mapM charToPlant
+
+charToPlant :: Char -> Maybe Plant
+charToPlant s = M.lookup s dict
+  where
+    dict = M.fromList [
+          ('C', Clover)
+        , ('G', Grass)
+        , ('R', Radishes)
+        , ('V', Violets)
+      ]
