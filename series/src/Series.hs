cf9633875f1c1ff249989c7c37f1462613e22098
Dmitry Kaysin <kaysin.dmitry@gmail.com>
2020-01-24 15:11:03 +0300

    Add solution for series

---
+++ series/src/Series.hs
@@ -0,0 +1,7 @@
+module Series (slices) where
+
+import           Data.Char (digitToInt)
+import           Data.List (tails)
+
+slices :: Int -> String -> [[Int]]
+slices n = filter ((==n).length) . map (take n) . tails . map digitToInt
